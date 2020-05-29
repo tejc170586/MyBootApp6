3997852ebc31ce4eef036d3ed8f3b040ae0eeb72
tejc170586 <aliendenpa1109@gmail.com>
2020-05-29 10:39:54 +0900

    Initial commit

---
+++ src/main/java/jp/te4a/spring/boot/myapp6/MyBootApp6Application.java
@@ -0,0 +1,13 @@
+package jp.te4a.spring.boot.myapp6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBootApp6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBootApp6Application.class, args);
+	}
+
+}
---
+++ src/test/java/jp/te4a/spring/boot/myapp6/MyBootApp6ApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.te4a.spring.boot.myapp6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBootApp6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

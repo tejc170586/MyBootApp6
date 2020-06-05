dd242c4de7463192010ab112567226b943d0ae14
170586 <170586@E0412-13.JC-21.local>
2020-06-05 12:48:51 +0900

    Changing kadai6 for creating kadai8

--- src/main/java/jp/te4a/spring/boot/myapp8/BookRepository.java
+++ src/main/java/jp/te4a/spring/boot/myapp8/BookRepository.java
@@ -1,18 +1,29 @@
-package jp.te4a.spring.boot.myapp6;
+package jp.te4a.spring.boot.myapp8;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class BookRepository{
 	private final ConcurrentMap<Integer, BookBean> bookMap
 									= new ConcurrentHashMap<>();
-	
-	public BookBean save(BookBean bookBean) {
+	private int BOOK_ID = 1;
+	public int getBookId() {
+		return BOOK_ID++;
+	}
+	public BookBean create(BookBean bookBean) {
 		return bookMap.put(bookBean.getId(), bookBean);
+	}
+	
+	public BookBean update(BookBean updateBookBean) {
+		BookBean bookBean = bookMap.get(updateBookBean.getId());
+		BeanUtils.copyProperties(updateBookBean, bookBean);
+		return bookBean;
 	}
 	
 	public void delete(Integer bookId) {
@@ -21,5 +32,9 @@ public class BookRepository{
 	
 	public List<BookBean> findAll(){
 		return new ArrayList<>(bookMap.values());
+	}
+	
+	public BookBean findOne(Integer id) {
+		return bookMap.get(id);
 	}
 }
--- src/test/java/jp/te4a/spring/boot/myapp8/BookApplicationTests.java
+++ src/test/java/jp/te4a/spring/boot/myapp8/BookApplicationTests.java
@@ -1,10 +1,10 @@
-package jp.te4a.spring.boot.myapp6;
+package jp.te4a.spring.boot.myapp8;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MyBootApp6ApplicationTests {
+class BookApplicationTests {
 
 	@Test
 	void contextLoads() {
